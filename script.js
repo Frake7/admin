adf62758e0344393ee7b00d54f925c69e7a47e73
Aleksei <5f4ff4f@gmail.com>
2019-12-01 13:10:31 +0200

    Работа н1

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-console.log("Hello world!");
-let string = "Test string what increace my chance to work with gitHub";
+let money = prompt("Ваш бюджет на месяц?");
+let timeDate = prompt("Введите дату в формате YYYY-MM-DD");
+let appData = {
+    money,
+    timeDate,
+    expenses: {},
+    optionalExpenses: 0,
+    income: [],
+    savings: false
+}
+optionalExpenses = prompt("Введите обязательную статью расходов в этом месяце");
+income = prompt("Во сколько обойдется?");
+expenses: {
+    optionalExpenses,
+    income
+}
+
+alert("Бюджет на 1 день равен: " + appData.money / 30);
