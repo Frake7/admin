ace69bf29066d3b06bb3820c8f27a4611ff7fa8c
Aleksei <5f4ff4f@gmail.com>
2019-12-06 20:48:22 +0200

    last push

--- script.js
+++ script.js
@@ -1,29 +1,108 @@
-let money = prompt("Ваш бюджет на месяц?");
-let timeDate = prompt("Введите дату в формате YYYY-MM-DD");
-let appData = {
-    money,
-    timeDate,
-    expenses: {},
-    optionalExpenses: 0,
-    income: [],
-    savings: false
+'use strict'
+
+let money,time,depo_dialog,DayBudget;
+function StartQuestions (){
+    money = +prompt ("Ваш бюджет на месяц?", "20000");
+    if(isNaN(money) || money == "null" || money == ""){
+        StartQuestions(); 
+    } else{
+        time = +prompt ("Введите дату в формате YYYY MM DD", "20191206");
+        if (isNaN(time) || time == "null" || time == ""){
+            StartQuestions();
+        }
+        
+    }
 }
 
-let i = 0;
-while (i < 2){
-    let a1 = +prompt("Введите обязательную статью расходов в этом месяце"),
-    a2 = +prompt("Во сколько обойдется?");
-    if (typeof(a1) != "number" || typeof(a2) != "number"){
-        console.log("error");
+// StartQuestions();
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+
+
+// console.log(appData.budget, appData.timeData);   
+function deposit(){
+    if (appData.savings == false){
+        depo_dialog = confirm ("У Вас нет депозита, желаете ли открыть его?", "");
+    }
+    if (depo_dialog){
+        let Percent = +prompt("Отлично! Теперь укажите под какой процент Вы хотите взять депозит (от 5 до 20 в месяц)", "20");
+        if (isNaN(Percent) || Percent < 5 || Percent > 20 || Percent == ""){
+            deposit();
+        } else{
+            let sum = appData.budget / 100 * Percent + appData.budget; // budget + Percent of deposit
+            alert("Отлично! Ровно через 30 дней Ваш буджет будет: " + sum, "");
+        }
+
+        
     } else{
-
+        console.log("false");
+    }
     
-    if (a1 == '' || a2 == '' ){
-        console.log("error not ''");
-    } else {
-    i++;
-    }
-    }
-} 
+}
+// deposit();
 
-alert("Бюджет на 1 день равен: " + appData.money / 30);
+function detectDayBudget(){
+    DayBudget = appData.budget / 30;
+    alert("Ваш дневной бюджет равен: ~" + Math.round(DayBudget) + " рублей.");
+}
+// detectDayBudget();
+
+function detectLevel(){
+    if (DayBudget <= 250){
+        alert("Низкий уровень дохода!");
+    }else if(DayBudget <= 1500){
+        alert("Средний уровень дохода!");
+    } else if(DayBudget > 1500){
+        alert("Доход выше среднего уровня!");
+    }
+}
+// detectLevel();
+
+function chooseOptExpenses(){
+    for(let i = 0; i < 3; i++){
+        let QuestExpensions = prompt("Статья необязательных расходов?", "10000");
+        appData.optionalExpenses[i] = QuestExpensions;
+    }
+}
+// chooseOptExpenses()
+
+
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {                            
+//         console.log ("bad result");
+//         i--;
+//     }
+
+// };
+
+// appData.moneyPerDay = appData.budget / 30;
+
+
+// alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+
+
+// if (appData.moneyPerDay < 100) {
+//     console.log ("Это минимальный уровень достатка!");
+// } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+//     console.log ("Это средний уровень достатка!");
+// } else if (appData.moneyPerDay > 2000) {
+//     console.log ("Это высокий уровень достатка!");
+// } else {
+//     console.log ("Произошла ошибка");
+// }
---
+++ work.js
@@ -0,0 +1,48 @@
+let obj = {
+    first : "first test message",
+    secound : "secound message ;3", 
+    third : {
+        f1 : "test1",
+        f2 : "test2"
+    }
+}
+
+
+let array = [67,34,12,74,12];
+array.pop();
+console.log(array);
+setTimeout(function(){
+    for(let i = 0; i < 5; i++){
+        console.log(array[i]);
+    }
+},500);
+
+
+
+
+
+
+
+// function ShowMyMessage(text){
+//     alert(text);
+// }
+
+// ShowMyMessage("Hi mas :)");
+
+// function Time(){
+// setTimeout(function(){
+//     console.log("test");
+// }, 500);
+// }
+// Time();
+
+// function  call (variable, callback){
+//     console.log(variable);
+//     callback();
+// }
+
+// function donuts(){
+//     console.log("Проверка калбэка x2");
+// }
+
+// call("Проверка callback [1]", donuts);
